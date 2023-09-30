7108934f0df7fb763f82da5734125662570da28a
dhaneshragu <dhanesh.v@iitg.ac.in>
2023-10-01 01:53:14 +0530

    python api complete

--- python/app/main.py
+++ python/app/main.py
@@ -1,3 +1,4 @@
+from sentence_transformers import SentenceTransformer
 from fastapi import FastAPI
 from pydantic import BaseModel
 import elasticsearch
@@ -5,31 +6,49 @@ from elasticsearch import Elasticsearch
 from elasticsearch import helpers
 import pandas as pd
 
+CSV_BASEPATH = "./data/"
+
 app = FastAPI()
 
-    
-class Details(BaseModel):
-    csv_path : str
-    elastic_username : str
-    elastic_password : str
-    index_name : str
 
-from sentence_transformers import SentenceTransformer
+class Details(BaseModel):
+    csv_name: str
+    elastic_username: str
+    elastic_password: str
+    elastic_endpoint: str
+    index_name: str
+
+
 model = SentenceTransformer('all-MiniLM-L6-v2')
+
 
 def get_embeddings(text):
     return model.encode(text)
 
 
-@app.get("/python")
+def start_es(endpoint, username, password):
+    es = Elasticsearch(endpoint,basic_auth=(username, password))
+    return [es.ping(),es]
+
+
+@app.get("/")
 def home():
-    return {"msg":"Welcome to Python Endpoint of Intranet Search Engine."}
+    return {"msg": "Welcome to Python Endpoint of Intranet Search Engine."}
 
 
 @app.post("/python/store", description="Python endpoint to get word embeddings from CSV and storing it in elasticsearch database.")
-def store(payload: Details):
-    df = pd.read_csv(Details.csv_path)
-    df = df.dropna(how='all') # Dropping all null valued rows
-    df['embeddings'] = df['content'].apply(get_embeddings) # Assuming the scraped content is inside content column
-    
-    return {"msg": "Successfully stored in elastic-search database"}
+def store(req: Details):
+    l = start_es(req.elastic_endpoint, req.elastic_username, req.elastic_password)
+    if not l[0]:
+        return {"msg": "elastic-Client initialisation failed", "data":l[0]}
+    es = l[1]
+    df = pd.read_csv(CSV_BASEPATH+req.csv_name+".csv")
+    df = df.dropna(how='all')  # Dropping all null valued rows
+    print(df.head())
+    print(df.columns)
+    # TODO: Include stop-words, NER for better processing
+
+    # ? CSV Structure : content,url,embeddings
+    df['embeddings'] = df['content'].apply(get_embeddings)
+    print(df["embeddings"])
+    return {"msg": "Successfully stored in elastic-search database", "embeddings":df['embeddings'].to_list}
