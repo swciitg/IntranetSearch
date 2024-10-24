dce613dabe23fe571e46b94e8eb884aec735885d
TSP06 <tanusiwach2005@gmail.com>
2024-10-25 03:53:04 +0530

    added routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,18 @@
-import Navbar from './navbar'
-import Footer from './footer'
-import Content from './content'
-import Bookmarks from './bookmarks'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import { Link } from 'react-router-dom'
-import React, { useState, useEffect } from 'react'
-import Darkmode from './darkmode'
-import MainContent from './components/MainContent'
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './navbar';
+import Footer from './footer';
+import Content from './content';
+import Bookmarks from './bookmarks';
+import Darkmode from './darkmode';
+import MainContent from './components/MainContent';
 
 function App() {
     // darkmode
-    const [darkMode, setDarkMode] = useState(false)
+    const [darkMode, setDarkMode] = useState(false);
     const toggleDarkMode = () => {
         setDarkMode(!darkMode);
-    }
+    };
 
     // bookmarks
     const [isVisible, setIsVisible] = useState(true);
@@ -29,9 +28,9 @@ function App() {
     };
 
     const handleMouseLeave = () => {
-      setTimeout(() => {
-        setStatea(false);
-    }, 500);
+        setTimeout(() => {
+            setStatea(false);
+        }, 500);
     };
 
     const handleBookmarkEnter = () => {
@@ -39,28 +38,48 @@ function App() {
     };
 
     const handleBookmarkLeave = () => {
-      setTimeout(() => {
-        setStateb(false);
-    }, 500);
+        setTimeout(() => {
+            setStateb(false);
+        }, 500);
     };
 
     const [showNewComponent, setShowNewComponent] = useState(false);
 
     return (
-        <div className={darkMode ? 'dark' : ''}>
-            <div className="flex flex-col justify-between items-center h-screen w-screen bg-primarycolor dark:bg-secondarycolor">
-                <div className="w-11/12 h-3/4 flex flex-col items-center justify-between">
-                    <Navbar onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}  showNewComponent={showNewComponent} />
-                    <Darkmode onToggle={toggleDarkMode} />
-                    <Content />
+        <Router>
+            <div className={darkMode ? 'dark' : ''}>
+                <div className="flex flex-col justify-between items-center h-screen w-screen bg-primarycolor dark:bg-secondarycolor">
+                    <div className="w-11/12 h-3/4 flex flex-col items-center justify-between">
+                        {/* Always display Navbar and Darkmode toggle */}
+                        <Navbar onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} showNewComponent={showNewComponent} />
+                        <Darkmode onToggle={toggleDarkMode} />
+
+                        {/* Routing Setup */}
+                        <Switch>
+                            {/* Route for Home (/) */}
+                            <Route exact path="/">
+                                <Content />
+                            </Route>
+
+                            {/* Route for Search (/search) */}
+                            <Route path="/search">
+                                <MainContent isDark={darkMode} />
+                            </Route>
+                        </Switch>
+                    </div>
+
+                    {/* Bookmarks show on both routes */}
+                    <div>
+                        {showNewComponent && (
+                            <Bookmarks onMouseEnter={handleBookmarkEnter} onMouseLeave={handleBookmarkLeave} />
+                        )}
+                    </div>
+
+                    {/* Always display Footer */}
+                    <Footer />
                 </div>
-                <div>
-                    {showNewComponent && <Bookmarks onMouseEnter={handleBookmarkEnter} onMouseLeave={handleBookmarkLeave} />}
-                </div>
-                <MainContent isDark={darkMode} />
-                <Footer />
             </div>
-        </div>
+        </Router>
     );
 }
 
