f7656420bef720d60fc6ee869d06430a3ae4fc98
unknown <priyagawshinde2601@gmail.com>
2024-10-26 16:59:38 +0530

    "layout changed ; routes changed ; pages and compo nents segregated ; footer links added"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,65 +1,52 @@
-import Navbar from './navbar'
-import Footer from './footer'
-import Content from './content'
-import Bookmarks from './bookmarks'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Navbar from './Components/navbar'
+import Footer from './Components/footer'
+import Content from './Components/content'
+import Bookmarks from './Components/bookmarks'
+import Layout from './pages/Layout'
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import { Link } from 'react-router-dom'
 import React, { useState, useEffect } from 'react'
 import Darkmode from './darkmode'
-
+import MostVisited from './Components/mostVisited'
+import RecentSearches from './Components/recentSearches'
+import ReactDOM from "react-dom/client";
 function App() {
     // darkmode
-    const [darkMode, setDarkMode] = useState(false)
-    const toggleDarkMode = () => {
-        setDarkMode(!darkMode);
-    }
-
-    // bookmarks
-    const [isVisible, setIsVisible] = useState(true);
-    const [statea, setStatea] = useState(false);
-    const [stateb, setStateb] = useState(false);
-
-    useEffect(() => {
-        setShowNewComponent(statea || stateb);
-    }, [statea, stateb]);
-
-    const handleMouseEnter = () => {
-        setStatea(true);
-    };
-
-    const handleMouseLeave = () => {
-      setTimeout(() => {
-        setStatea(false);
-    }, 500);
-    };
-
-    const handleBookmarkEnter = () => {
-        setStateb(true);
-    };
-
-    const handleBookmarkLeave = () => {
-      setTimeout(() => {
-        setStateb(false);
-    }, 500);
-    };
-
-    const [showNewComponent, setShowNewComponent] = useState(false);
 
     return (
-        <div className={darkMode ? 'dark' : ''}>
-            <div className="flex flex-col justify-between items-center h-screen w-screen bg-primarycolor dark:bg-secondarycolor">
-                <div className="w-11/12 h-3/4 flex flex-col items-center justify-between">
-                    <Navbar onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}  showNewComponent={showNewComponent} />
-                    <Darkmode onToggle={toggleDarkMode} />
-                    <Content />
-                </div>
-                <div>
-                    {showNewComponent && <Bookmarks onMouseEnter={handleBookmarkEnter} onMouseLeave={handleBookmarkLeave} />}
-                </div>
-                <Footer />
-            </div>
-        </div>
+
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={< Layout/> }>
+          {/* <Route index element={<Home />} /> */}
+          {/* <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} /> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+
+    
+       
     );
 }
 
 export default App;
+
+// export default function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<Layout />}>
+//           <Route index element={<Home />} />
+//           <Route path="blogs" element={<Blogs />} />
+//           <Route path="contact" element={<Contact />} />
+//           <Route path="*" element={<NoPage />} />
+//         </Route>
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
